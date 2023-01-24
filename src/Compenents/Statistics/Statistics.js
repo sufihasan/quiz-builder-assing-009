5378f62d9f6f23764ff09da06336b95e20c7922b
sufihasan <hasan6nh@gmail.com>
2023-01-24 14:07:33 +0600

    work in statistics

--- src/Compenents/Statistics/Statistics.js
+++ src/Compenents/Statistics/Statistics.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from 'recharts';
 
 const Statistics = () => {
+    const quizes = useLoaderData().data;
+    console.log('ppp', quizes);
     return (
-        <div>
+        <div className='container mt-4'>
             <p>this is statistics</p>
+            <LineChart width={600} height={300} data={quizes} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+                <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                <XAxis dataKey="name" />
+                <YAxis />
+            </LineChart>
         </div>
     );
 };

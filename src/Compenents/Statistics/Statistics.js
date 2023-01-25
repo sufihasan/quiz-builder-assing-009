6c2915c63c35875d2801a36744d8d4bc9ea7328d
sufihasan <hasan6nh@gmail.com>
2023-01-25 14:46:48 +0600

    delete some unessary thing

--- src/Compenents/Statistics/Statistics.js
+++ src/Compenents/Statistics/Statistics.js
@@ -18,18 +18,7 @@ const Statistics = () => {
                 </LineChart>
             </ResponsiveContainer>
 
-            {/* <ResponsiveContainer width={700} height="80%">
-    <AreaChart data={data}
-      margin={{ top: 20, right: 30, left: 0, bottom: 0 }}>
-      <XAxis dataKey="name" />
-      <YAxis />
-      <CartesianGrid strokeDasharray="3 3" />
-      <Tooltip />
-      <ReferenceLine x="Page C" stroke="green" label="Min PAGE" />
-      <ReferenceLine y={4000} label="Max" stroke="red" strokeDasharray="3 3" />
-      <Area type="monotone" dataKey="uv" stroke="#8884d8" fill="#8884d8" />
-    </AreaChart>
-  </ResponsiveContainer> */}
+
         </div >
     );
 };
